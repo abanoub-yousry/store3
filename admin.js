fa6c5fbc3cd69bcdb34d18248c44737ff0ac578c
abanoub-yousry <157910293+abanoub-yousry@users.noreply.github.com>
2025-03-14 17:14:45 +0200

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,34 @@
+// ربط Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDsN3Nlx3TVxz4zrvY06NX9RR2DTVDCREU",
+    authDomain: "dora-store.firebaseapp.com",
+    projectId: "dora-store",
+    storageBucket: "dora-store.firebasestorage.app",
+    messagingSenderId: "331430680486",
+    appId: "1:331430680486:web:bf9c5880015fde63496167",
+    measurementId: "G-YF9G38JB8V"
+  };
+  
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+// إضافة منتج
+const form = document.getElementById("productForm");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("productName").value;
+    const price = document.getElementById("productPrice").value;
+    const image = document.getElementById("productImage").value;
+
+    db.ref("products").push({
+        name: name,
+        price: price,
+        image: image
+    });
+
+    alert("تمت إضافة المنتج بنجاح!");
+    form.reset();
+});
