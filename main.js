210228325000cdad5ac2e3ba11de9235a56e77d5
abanoub-yousry <157910293+abanoub-yousry@users.noreply.github.com>
2025-03-15 01:37:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+import { getFirestore, collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDsN3Nlx3TVxz4zrvY06NX9RR2DTVDCREU",
   authDomain: "dora-store.firebaseapp.com",
-  databaseURL: "https://dora-store-default-rtdb.firebaseio.com",
   projectId: "dora-store",
   storageBucket: "dora-store.appspot.com",
   messagingSenderId: "331430680486",
   appId: "1:331430680486:web:bf9c5880015fde63496167",
-  measurementId: "G-YF9G38JB8V",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const storage = getStorage(app);
 
 const proContainer = document.querySelector(".pro-container");
 
 async function requestAndBuild(category = "all") {
-    proContainer.innerHTML = ""; // تفريغ المحتوى قبل الإضافة
+    proContainer.innerHTML = "";
 
     let q = category === "all" ? collection(db, "products") : query(collection(db, "products"), where("category", "==", category));
 
@@ -28,19 +24,18 @@ async function requestAndBuild(category = "all") {
 
     querySnapshot.forEach((doc) => {
         const product = doc.data();
-        console.log(product); // تحقق من البيانات
         createProductCard(product);
     });
 }
 
 function createProductCard(product) {
-    const { name, price, imageUrl, category } = product;
+    const { title, price, imageURL, category } = product;
 
     const productHTML = `
         <div class="pro">
-            <img src="${imageUrl}" alt="${name}" onerror="this.src='placeholder-image.jpg';">
+            <img src="${imageURL}" alt="${title}" onerror="this.src='placeholder-image.jpg';">
             <div class="description">
-                <h3>${name}</h3>
+                <h3>${title}</h3>
                 <h4>${category}</h4>
                 <h4>$${price}</h4>
             </div>
@@ -51,10 +46,8 @@ function createProductCard(product) {
     proContainer.innerHTML += productHTML;
 }
 
-// تحميل المنتجات عند بدء الصفحة
 requestAndBuild();
 
-// التعامل مع أزرار التصفية
 const filterButtons = document.querySelectorAll(".filter-btn");
 filterButtons.forEach((button) => {
     button.addEventListener("click", () => {
